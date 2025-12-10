ba07a84c2b6601a1da71c6ee49805b21a6701fb8
JoeDod <592910954@qq.com>
2025-12-10 15:43:45 +0800

    重构 SimpleShowText 节点的 onNodeCreated 方法，优化 widget 创建和更新逻辑

--- js/show_text.js
+++ js/show_text.js
@@ -5,24 +5,29 @@ app.registerExtension({
 	name: "Comfy.SimpleHttpRequest.ShowText",
 	async beforeRegisterNodeDef(nodeType, nodeData, app) {
 		if (nodeData.name === "SimpleShowText") {
-			const onNodeCreated = nodeType.prototype.onNodeCreated;
-			nodeType.prototype.onNodeCreated = function () {
-				const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
-
-				const w = ComfyWidgets["STRING"](this, "text", ["STRING", { multiline: true }], app).widget;
-				w.inputEl.readOnly = true;
-				w.inputEl.style.opacity = 0.6;
-				return r;
-			};
-
 			const onExecuted = nodeType.prototype.onExecuted;
 			nodeType.prototype.onExecuted = function (message) {
 				onExecuted?.apply(this, arguments);
 
 				if (message && message.text) {
-					const w = this.widgets.find((w) => w.name === "text");
+					// 尝试找到名为 "display_text" 的 widget
+					let w = this.widgets && this.widgets.find((w) => w.name === "display_text");
+
+					// 如果没找到，创建一个新的
+					if (!w) {
+						// ComfyWidgets["STRING"] 返回的是一个对象 { widget: ... }
+						// 参数: node, name, [type, options], app
+						const widgetData = ComfyWidgets["STRING"](this, "display_text", ["STRING", { multiline: true }], app);
+						w = widgetData.widget;
+						w.inputEl.readOnly = true;
+						w.inputEl.style.opacity = 0.6;
+					}
+
+					// 更新值
 					if (w) {
 						w.value = message.text.join("");
+						// 调整节点大小以适应内容（可选）
+						this.onResize?.(this.size);
 					}
 				}
 			};
